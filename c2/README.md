dad811b6d3df01b1eeddf5fe609ddcc9acbbfe95
MusicDev <smccowan33@gmail.com>
2025-06-10 21:30:31 -0700

    Added c2

---
+++ c2/README.md
@@ -0,0 +1,18 @@
+# Challenge 2 - A Simple Terminal Interface
+Applications start to get much more useful when you can make a commandline interface. So that's what we're doing!
+
+We get the same data as before, but we're going to run our script like this:
+
+`python3 main.py username`
+
+Let's se what this might look like for Sophie Bennett:
+
+`python main.py sbennett`
+
+Output:
+
+```
+Sophie Bennett
+  Username: sbennett
+  Location: London, UK
+```
---
+++ c2/main.py
@@ -0,0 +1,52 @@
+'''
+Challenge 2 - 
+'''
+USERS = [
+  {
+    "name": "Seongwoo Kim",
+    "username": "skim",
+    "location": "112"
+  },
+  {
+    "name": "Sophia Bennett",
+    "username": "sbennett",
+    "location": "43"
+  },
+  {
+    "name": "Leo Fernandez",
+    "username": "lfernandez",
+    "location": "118"
+  },
+  {
+    "name": "Gabriel Santos",
+    "username": "gsantos",
+    "location": "341"
+  },
+  {
+    "name": "Zhurong Chen",
+    "username": "czhurong",
+    "location": "203"
+  },
+  {
+    "name": "Maya Rodriguez",
+    "username": "mrodriguez",
+    "location": "35"
+  },
+  {
+    "name": "Srithi Patel",
+    "username": "spatel",
+    "location": "117"
+  },
+]
+
+LOCATION_MAP = {
+  "35": "Toronto, Canada",
+  "43": "London, UK",
+  "112": "Seoul, South Korea",
+  "117": "Mumbai, India",
+  "118": "Buenos Aires, Argentina",
+  "203": "Shanghai, China",
+  "341": "Rio de Janeiro, Brazil",
+}
+
+# Write your code below
